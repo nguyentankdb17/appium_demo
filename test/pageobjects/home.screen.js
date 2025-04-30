a93be294da84dfee321e104d728db74473ef68a2
nguyentankdb17 <nguyentankdb17@gmail.com>
2025-04-30 17:26:05 +0700

    update elements

--- test/pageobjects/home.screen.js
+++ test/pageobjects/home.screen.js
@@ -1,9 +1,43 @@
 class HomeScreen {
+    get homeButton() {
+      return $('//*[@text="Home"]');
+    }
+    
     get profileButton() {
       return $('//*[@text="Profile"]');
     }
+
     get loginButton() {
-      return $('//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.view.View[1]/android.widget.Button');
+      return $('//*[@text="Login"]');
+    }
+
+    get signUpButton() {
+      return $('//*[@text="Sign Up"]');
+    }
+
+    get searchBar() {
+      return $('//android.widget.EditText/android.view.View');
+    }
+
+    get welcomeBackMessgae() {
+      return $('//*[@text="Welcome back"]');
+    }
+
+    get firstSearchResult() {
+      return $('//android.widget.ScrollView/android.view.View[3]/android.view.View');
+    }
+
+    async searchMovie(movieName) {
+      await this.searchBar.click();
+      await this.searchBar.clearValue();
+      await browser.waitUntil(
+          async () => (await this.searchBar.getText()) === '',
+          {
+              timeout: 3000,
+              timeoutMsg: 'Search bar not cleared in time'
+          }
+      );
+      await this.searchBar.sendKeys(movieName);
     }
   }
   
